95a19ee4a7bb3aae92b30b218b0576aa3b53658c
Bexruz Xolmuminov <54550270+bexruzbek@users.noreply.github.com>
2020-11-04 13:14:48 +0500

    Update keys.js

--- build/src/keys.js
+++ build/src/keys.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mongo = void 0;
 exports.mongo = {
-    "URI": "mongodb+srv://bexruz:1b2b3b4b5b6b7b@devcamper.iiz3v.mongodb.net/booksTS?retryWrites=true&w=majority"
+    "URI": "your uri"
 };
